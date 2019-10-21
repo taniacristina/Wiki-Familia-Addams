541dfd7a88e5110c0984f7bd5bc64add6e8a00df
Tânia Cristina <taniacristinasantosrocha@gmail.com>
2019-10-21 17:14:31 -0300

    Adicionando filmes.js

--- Versão 1/JS/filmes.js
+++ Versão 1/JS/filmes.js
@@ -17,16 +17,9 @@ function carregarDados() {
 function criaFilmes(xml) {
     var xmlDoc = xml.responseXML;
     var x = xmlDoc.getElementsByTagName("FILME");
-    var filmes = document.getElementsByTagName("body");
+    var filmes = document.getElementById("corpo");
 
     for(var i = 0; i < x.length; i++) {
-        // console.log(x[i].getElementsByTagName("TITULO")[0].childNodes[0].nodeValue);
-        // console.log(x[i].getElementsByTagName("DATA")[0].childNodes[0].nodeValue);
-        // console.log(x[i].getElementsByTagName("IMAGEM")[0].childNodes[0].nodeValue);
-        // console.log(x[i].getElementsByTagName("DESCRICAO")[0].childNodes[0].nodeValue);
-        // console.log(x[i].getElementsByTagName("RESUMO")[0].childNodes[0].nodeValue);
-        // console.log(x[i].getElementsByTagName("PERSONAGENS")[0].childNodes[0].nodeValue);
-        // console.log(x[i].getElementsByTagName("LINK")[0].childNodes[0].nodeValue);
 
         var section = document.createElement("section");
         var h2 = document.createElement("h2");
@@ -35,16 +28,51 @@ function criaFilmes(xml) {
         var figure = document.createElement("figure");
         var img = document.createElement("img");
         var figCaption = document.createElement("figcaption");
+        var video = document.createElement("video");
+        var pVideo = document.createElement("p");
+        var pDescricao = document.createElement("p");
         var h4 = document.createElement("Resumo");
         var pResumo = document.createElement("p");
-        var personagens = document.createComment("p");
+        var personagens = document.createElement("p");
         var strong = document.createElement("strong");
         var a = document.createElement("a");
 
         h2.innerHTML = x[i].getElementsByTagName("TITULO")[0].childNodes[0].nodeValue;
-        pData.class
+        pData.classList.add("centralizar");
+        em.innerHTML = x[i].getElementsByTagName("DATA")[0].childNodes[0].nodeValue;
+        pData.appendChild(em);
+        img.src = x[i].getElementsByTagName("IMAGEM")[0].childNodes[0].nodeValue;
+        img.width = 200;
+        img.height = 300;
+        figCaption.innerHTML = x[i].getElementsByTagName("LEGENDA")[0].childNodes[0].nodeValue;
+        figure.appendChild(img);
+        figure.appendChild(figCaption);
+        pVideo.classList.add("centralizar");
+        video.width = 300;
+        video.height = 200;
+        video.src = x[i].getElementsByTagName("VIDEO")[0].childNodes[0].nodeValue;
+        pVideo.appendChild(video);
+        pDescricao.innerHTML = x[i].getElementsByTagName("DESCRICAO")[0].childNodes[0].nodeValue
+        pDescricao.classList.add("centralizar");
+        h4.innerHTML = "Resumo";
+        pResumo.classList.add("justificar");
+        pResumo.innerHTML = x[i].getElementsByTagName("RESUMO")[0].childNodes[0].nodeValue;
+        strong.innerHTML = "Personagens principais participantes: ";
+        personagens.appendChild(strong);
+        personagens.innerHTML = x[i].getElementsByTagName("PERSONAGENS")[0].childNodes[0].nodeValue;
+        a.target = "_blank";
+        a.href = x[i].getElementsByTagName("LINK")[0].childNodes[0].nodeValue;
 
+        section.appendChild(h2);
+        section.appendChild(pData);
+        section.appendChild(figure);
+        section.appendChild(pVideo);
+        section.appendChild(pDescricao);
+        section.appendChild(h4);
+        section.appendChild(pResumo);
+        section.appendChild(personagens);
+        section.appendChild(a);
         filmes.appendChild(section);
-        
+
     }
 }
